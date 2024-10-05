efb1a0b82c36abbcb6bc5ee5ef26a02b9ec41cdb
devemre <dev.emre.aydin@gmail.com>
2024-10-05 13:15:58 +0300

    chore: cleaning frontend

--- library-management-frontend/src/App.tsx
+++ library-management-frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './components';
+import { Home } from './pages';
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    id: 'root',
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        id: 'home',
+        path: '/',
+        element: <Home />,
+      },
+      {
+        id: 'books',
+        path: '/books',
+        element: <div>Books</div>,
+      },
+      {
+        id: 'users',
+        path: '/users',
+        element: <div>Users</div>,
+      },
+      {
+        id: 'book-details',
+        path: '/books/:id',
+        element: <div>BookDetails</div>,
+      },
+      {
+        id: 'user-details',
+        path: '/users/:id',
+        element: <div>UserDetails</div>,
+      },
+    ],
+  },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
---
+++ library-management-frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+import BookDetails from './BookDetails';
+import Books from './Books';
+import Home from './Home';
+import UserDetails from './UserDetails';
+import Users from './Users/Users';
+
+export { BookDetails, Books, Home, UserDetails, Users };
